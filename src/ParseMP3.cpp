6287074264d862a8abc0cdd29c3ea37d44d0c1b0
markus <markus>
2006-07-18 02:11:04 +0000

    Also parsing files with ID3v2 tag

--- src/ParseMP3.cpp
+++ src/ParseMP3.cpp
@@ -8,7 +8,7 @@
 //REVISION    : $Revision$
 //AUTHOR      : Markus Schwab
 //CREATED     : 15.01.2003
-//COPYRIGHT   : Copyright (C) 2003 - 2005
+//COPYRIGHT   : Copyright (C) 2003 - 2006
 
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -27,6 +27,7 @@
 
 #include <YGP/Trace.h>
 
+#include "Utility.h"
 #include "Properties.h"
 
 #include "ParseMP3.h"
@@ -51,21 +52,64 @@ ParseMP3::~ParseMP3 () {
 /// \param result: Out: Found information
 //-----------------------------------------------------------------------------
 void ParseMP3::parse (YGP::Xistream& stream, Properties& result) {
-   stream.seekg (-0x80, std::ios::end);
-   std::string value;
+   char buffer[10];
+   stream.read (buffer, sizeof (buffer));
+   if (memcmp (buffer, "ID3", 3)) {
+      stream.seekg (-0x80, std::ios::end);
 
-   std::getline (stream, value, '\xff');
-   TRACE9 ("ParseMP3::parse (Xistream&, Properties&) - Found: " << value
-           << "; Length: " << value.size ());
-   if ((value[0] == 'T') && (value[1] == 'A') && (value[2] == 'G')) {
-      result.strComment = strip (value, 63, 29);
-      result.strTitle = strip (value, 3, 29);
-      result.strAuthor = strip (value, 33, 29);
+      std::string value;
+      std::getline (stream, value, '\xff');
+      TRACE9 ("ParseMP3::parse (Xistream&, Properties&) - Found: " << value
+	      << "; Length: " << value.size ());
+      if ((value[0] == 'T') && (value[1] == 'A') && (value[2] == 'G')) {
+	 result.strComment = strip (value, 63, 29);
+	 result.strTitle = strip (value, 3, 29);
+	 result.strAuthor = strip (value, 33, 29);
+      }
+   }
+   else {
+      unsigned int lenID3 (0);
+      for (unsigned int i (6); i < 10; ++i) {
+	 lenID3 <<= 7;
+	 lenID3 += buffer[i];
+      }
+      TRACE5 ("ParseMP3::parse (Xistream&, Properties&) - Len of ID3: " << lenID3);
+
+      char* id3 (new char [lenID3]);
+      const char* pos (id3);
+      stream.read (id3, lenID3);
+
+      while (static_cast<unsigned int> (pos - id3) < lenID3) {
+	 unsigned int len ((pos[4] << 21) + (pos[5] << 14) + (pos[6] << 7) + pos[7]);
+
+	 TRACE7 ("ParseMP3::parse (Xistream&, Properties&) - Frame: " << std::string (pos, 4));
+	 TRACE3 ("ParseMP3::parse (Xistream&, Properties&) - Len of frame: " << len);
+
+	 switch (get4BytesLSB (pos)) {
+	 case 0x32544954:                                           // TIT2-tag
+	    result.strTitle = std::string (pos + 11, len - 1);
+	    break;
+
+	 case 0x31455054:                                           // TPE1-tag
+	    result.strAuthor = std::string (pos + 11, len - 1);
+	    break;
+
+	 case 0x424C4154:                                           // TPE1-tag
+	    result.strComment = std::string (pos + 11, len - 1);
+	    break;
+
+	 case 0:
+	    return;
+	 } // end-switch
+
+	 pos += len + 10;
+      } // end-while
+      delete id3;
    }
 }
 
 //-----------------------------------------------------------------------------
-/// Returns the specified substring, removed from trailing spaces
+/// Returns the specified substring removed from trailing spaces
 /// \param value: String to manipulate
 /// \param pos: Starting pos inside the string
 /// \param len: Maximal length of string
